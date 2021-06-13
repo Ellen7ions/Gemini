79d51a0f9e6c9df79c66fa1c7a0f92ffb6a188c6
Ellen7ions <lzz765177255@gmail.com>
2021-06-13 10:52:10 +0800

    Optimize the route, but not enough. 50Mhz is fail...

--- rtl/lsu/lsu.v
+++ rtl/lsu/lsu.v
@@ -24,7 +24,7 @@ module lsu (
     output  wire [31:0] mem_alu_res,
     output  wire        mem_w_reg_ena,
     output  wire [4 :0] mem_w_reg_dst,
-    output  wire [31:0] mem_r_data,
+    output  reg  [31:0] mem_r_data,
     output  wire        mem_wb_reg_sel,
 
     output  wire [1 :0] mem_w_hilo_ena,
@@ -152,112 +152,208 @@ module lsu (
 
     // mem
 
-    wire [31:0] mem_lb_data ;
-    wire [31:0] mem_lbu_data;
-    wire [31:0] mem_lh_data ;
-    wire [31:0] mem_lhu_data;
-    wire [31:0] mem_lw_data ;
-    wire [31:0] mem_lwl_data;
-    wire [31:0] mem_lwr_data;
+    // wire [31:0] mem_lb_data ;
+    // wire [31:0] mem_lbu_data;
+    // wire [31:0] mem_lh_data ;
+    // wire [31:0] mem_lhu_data;
+    // wire [31:0] mem_lw_data ;
+    // wire [31:0] mem_lwl_data;
+    // wire [31:0] mem_lwr_data;
 
-    assign mem_lb_data = 
-            {32{
-                mem_alu_res[1:0] == 2'b00
-            }} & {{24{data_ram_rdata[ 7]}}, data_ram_rdata[7 : 0]} |
-            {32{
-                mem_alu_res[1:0] == 2'b01
-            }} & {{24{data_ram_rdata[15]}}, data_ram_rdata[15: 8]} |
-            {32{
-                mem_alu_res[1:0] == 2'b10
-            }} & {{24{data_ram_rdata[23]}}, data_ram_rdata[23:16]} |
-            {32{
-                mem_alu_res[1:0] == 2'b11
-            }} & {{24{data_ram_rdata[31]}}, data_ram_rdata[31:24]} ;
+    // assign mem_lb_data = 
+    //         {32{
+    //             mem_alu_res[1:0] == 2'b00
+    //         }} & {{24{data_ram_rdata[ 7]}}, data_ram_rdata[7 : 0]} |
+    //         {32{
+    //             mem_alu_res[1:0] == 2'b01
+    //         }} & {{24{data_ram_rdata[15]}}, data_ram_rdata[15: 8]} |
+    //         {32{
+    //             mem_alu_res[1:0] == 2'b10
+    //         }} & {{24{data_ram_rdata[23]}}, data_ram_rdata[23:16]} |
+    //         {32{
+    //             mem_alu_res[1:0] == 2'b11
+    //         }} & {{24{data_ram_rdata[31]}}, data_ram_rdata[31:24]} ;
 
-    assign mem_lbu_data =
-            {32{
-                mem_alu_res[1:0] == 2'b00
-            }} & {{24{1'b0}}, data_ram_rdata[7 : 0]} |
-            {32{
-                mem_alu_res[1:0] == 2'b01
-            }} & {{24{1'b0}}, data_ram_rdata[15: 8]} |
-            {32{
-                mem_alu_res[1:0] == 2'b10
-            }} & {{24{1'b0}}, data_ram_rdata[23:16]} |
-            {32{
-                mem_alu_res[1:0] == 2'b11
-            }} & {{24{1'b0}}, data_ram_rdata[31:24]} ;
+    // assign mem_lbu_data =
+    //         {32{
+    //             mem_alu_res[1:0] == 2'b00
+    //         }} & {{24{1'b0}}, data_ram_rdata[7 : 0]} |
+    //         {32{
+    //             mem_alu_res[1:0] == 2'b01
+    //         }} & {{24{1'b0}}, data_ram_rdata[15: 8]} |
+    //         {32{
+    //             mem_alu_res[1:0] == 2'b10
+    //         }} & {{24{1'b0}}, data_ram_rdata[23:16]} |
+    //         {32{
+    //             mem_alu_res[1:0] == 2'b11
+    //         }} & {{24{1'b0}}, data_ram_rdata[31:24]} ;
 
-    assign mem_lh_data  =
-            {32{
-                mem_alu_res[1:0] == 2'b00
-            }} & {{16{data_ram_rdata[15]}}, data_ram_rdata[15: 0]}   |
-            {32{
-                mem_alu_res[1:0] == 2'b10
-            }} & {{16{data_ram_rdata[31]}}, data_ram_rdata[31:16]}   ;
+    // assign mem_lh_data  =
+    //         {32{
+    //             mem_alu_res[1:0] == 2'b00
+    //         }} & {{16{data_ram_rdata[15]}}, data_ram_rdata[15: 0]}   |
+    //         {32{
+    //             mem_alu_res[1:0] == 2'b10
+    //         }} & {{16{data_ram_rdata[31]}}, data_ram_rdata[31:16]}   ;
 
-    assign mem_lhu_data =
-            {32{
-                mem_alu_res[1:0] == 2'b00
-            }} & {{16{1'b0}}, data_ram_rdata[15: 0]}   |
-            {32{
-                mem_alu_res[1:0] == 2'b10
-            }} & {{16{1'b0}}, data_ram_rdata[31:16]}   ;
+    // assign mem_lhu_data =
+    //         {32{
+    //             mem_alu_res[1:0] == 2'b00
+    //         }} & {{16{1'b0}}, data_ram_rdata[15: 0]}   |
+    //         {32{
+    //             mem_alu_res[1:0] == 2'b10
+    //         }} & {{16{1'b0}}, data_ram_rdata[31:16]}   ;
     
-    assign mem_lw_data  =
-            data_ram_rdata;
+    // assign mem_lw_data  =
+    //         data_ram_rdata;
 
-    assign mem_lwl_data =
-            {32{
-                mem_alu_res[1:0] == 2'b00
-            }} & {data_ram_rdata[7 :0], ex_mem_rt_data[23:0]}   |
-            {32{
-                mem_alu_res[1:0] == 2'b01
-            }} & {data_ram_rdata[15:0], ex_mem_rt_data[15:0]}   |
-            {32{
-                mem_alu_res[1:0] == 2'b10
-            }} & {data_ram_rdata[23:0], ex_mem_rt_data[7 :0]}   |
-            {32{
-                mem_alu_res[1:0] == 2'b11
-            }} & {data_ram_rdata[31:0]}                         ;
+    // assign mem_lwl_data =
+    //         {32{
+    //             mem_alu_res[1:0] == 2'b00
+    //         }} & {data_ram_rdata[7 :0], ex_mem_rt_data[23:0]}   |
+    //         {32{
+    //             mem_alu_res[1:0] == 2'b01
+    //         }} & {data_ram_rdata[15:0], ex_mem_rt_data[15:0]}   |
+    //         {32{
+    //             mem_alu_res[1:0] == 2'b10
+    //         }} & {data_ram_rdata[23:0], ex_mem_rt_data[7 :0]}   |
+    //         {32{
+    //             mem_alu_res[1:0] == 2'b11
+    //         }} & {data_ram_rdata[31:0]}                         ;
     
-    assign mem_lwr_data =
-            {32{
-                mem_alu_res[1:0] == 2'b00
-            }} & {data_ram_rdata[31:0]}                         |
-            {32{
-                mem_alu_res[1:0] == 2'b01
-            }} & {ex_mem_rt_data[31:24], data_ram_rdata[31: 8]}  |
-            {32{
-                mem_alu_res[1:0] == 2'b10
-            }} & {ex_mem_rt_data[31:16], data_ram_rdata[31:16]} |
-            {32{
-                mem_alu_res[1:0] == 2'b11
-            }} & {ex_mem_rt_data[31: 8], data_ram_rdata[31:24]} ;
+    // assign mem_lwr_data =
+    //         {32{
+    //             mem_alu_res[1:0] == 2'b00
+    //         }} & {data_ram_rdata[31:0]}                         |
+    //         {32{
+    //             mem_alu_res[1:0] == 2'b01
+    //         }} & {ex_mem_rt_data[31:24], data_ram_rdata[31: 8]}  |
+    //         {32{
+    //             mem_alu_res[1:0] == 2'b10
+    //         }} & {ex_mem_rt_data[31:16], data_ram_rdata[31:16]} |
+    //         {32{
+    //             mem_alu_res[1:0] == 2'b11
+    //         }} & {ex_mem_rt_data[31: 8], data_ram_rdata[31:24]} ;
 
-    assign mem_r_data =
-            {{32{ex_mem_ls_ena}}} & (({32{
-                ex_mem_ls_sel == `LS_SEL_LB
-            }} & mem_lb_data    )   |
-            ({32{
-                ex_mem_ls_sel == `LS_SEL_LBU
-            }} & mem_lbu_data   )   |
-            ({32{
-                ex_mem_ls_sel == `LS_SEL_LH
-            }} & mem_lh_data    )   |
-            ({32{
-                ex_mem_ls_sel == `LS_SEL_LHU
-            }} & mem_lhu_data   )   |
-            ({32{
-                ex_mem_ls_sel == `LS_SEL_LW
-            }} & mem_lw_data    )   |
-            ({32{
-                ex_mem_ls_sel == `LS_SEL_LWL
-            }} & mem_lwl_data   )   |
-            ({32{
-                ex_mem_ls_sel == `LS_SEL_LWR
-            }} & mem_lwr_data   ) 
-            );
+    // assign mem_r_data =
+    //         {{32{ex_mem_ls_ena}}} & (({32{
+    //             ex_mem_ls_sel == `LS_SEL_LB
+    //         }} & mem_lb_data    )   |
+    //         ({32{
+    //             ex_mem_ls_sel == `LS_SEL_LBU
+    //         }} & mem_lbu_data   )   |
+    //         ({32{
+    //             ex_mem_ls_sel == `LS_SEL_LH
+    //         }} & mem_lh_data    )   |
+    //         ({32{
+    //             ex_mem_ls_sel == `LS_SEL_LHU
+    //         }} & mem_lhu_data   )   |
+    //         ({32{
+    //             ex_mem_ls_sel == `LS_SEL_LW
+    //         }} & mem_lw_data    )   |
+    //         ({32{
+    //             ex_mem_ls_sel == `LS_SEL_LWL
+    //         }} & mem_lwl_data   )   |
+    //         ({32{
+    //             ex_mem_ls_sel == `LS_SEL_LWR
+    //         }} & mem_lwr_data   ) 
+    //         );
+    
+    always @(*) begin
+        case ({ex_mem_ls_ena, ex_mem_ls_sel})
+        {1'b1, `LS_SEL_LB   }: begin
+            mem_r_data  =
+                {32{
+                    mem_alu_res[1:0] == 2'b00
+                }} & {{24{data_ram_rdata[ 7]}}, data_ram_rdata[7 : 0]} |
+                {32{
+                    mem_alu_res[1:0] == 2'b01
+                }} & {{24{data_ram_rdata[15]}}, data_ram_rdata[15: 8]} |
+                {32{
+                    mem_alu_res[1:0] == 2'b10
+                }} & {{24{data_ram_rdata[23]}}, data_ram_rdata[23:16]} |
+                {32{
+                    mem_alu_res[1:0] == 2'b11
+                }} & {{24{data_ram_rdata[31]}}, data_ram_rdata[31:24]} ;
+        end
+
+        {1'b1, `LS_SEL_LBU  }: begin
+            mem_r_data  =
+                {32{
+                    mem_alu_res[1:0] == 2'b00
+                }} & {{24{1'b0}}, data_ram_rdata[7 : 0]} |
+                {32{
+                    mem_alu_res[1:0] == 2'b01
+                }} & {{24{1'b0}}, data_ram_rdata[15: 8]} |
+                {32{
+                    mem_alu_res[1:0] == 2'b10
+                }} & {{24{1'b0}}, data_ram_rdata[23:16]} |
+                {32{
+                    mem_alu_res[1:0] == 2'b11
+                }} & {{24{1'b0}}, data_ram_rdata[31:24]} ;
+        end
+
+        {1'b1, `LS_SEL_LH   }: begin
+            mem_r_data  =
+                {32{
+                    mem_alu_res[1:0] == 2'b00
+                }} & {{16{data_ram_rdata[15]}}, data_ram_rdata[15: 0]}   |
+                {32{
+                    mem_alu_res[1:0] == 2'b10
+                }} & {{16{data_ram_rdata[31]}}, data_ram_rdata[31:16]}   ;
+        end
+
+        {1'b1, `LS_SEL_LHU  }: begin
+            mem_r_data  =
+                {32{
+                    mem_alu_res[1:0] == 2'b00
+                }} & {{16{1'b0}}, data_ram_rdata[15: 0]}   |
+                {32{
+                    mem_alu_res[1:0] == 2'b10
+                }} & {{16{1'b0}}, data_ram_rdata[31:16]}   ;
+        end
+
+        {1'b1, `LS_SEL_LW   }: begin
+            mem_r_data  =
+                data_ram_rdata;
+        end
+
+        {1'b1, `LS_SEL_LWL  }: begin
+            mem_r_data  =
+                {32{
+                    mem_alu_res[1:0] == 2'b00
+                }} & {data_ram_rdata[7 :0], ex_mem_rt_data[23:0]}   |
+                {32{
+                    mem_alu_res[1:0] == 2'b01
+                }} & {data_ram_rdata[15:0], ex_mem_rt_data[15:0]}   |
+                {32{
+                    mem_alu_res[1:0] == 2'b10
+                }} & {data_ram_rdata[23:0], ex_mem_rt_data[7 :0]}   |
+                {32{
+                    mem_alu_res[1:0] == 2'b11
+                }} & {data_ram_rdata[31:0]}                         ;
+        end
+
+        {1'b1, `LS_SEL_LWR  }: begin
+            mem_r_data  =
+                {32{
+                    mem_alu_res[1:0] == 2'b00
+                }} & {data_ram_rdata[31:0]}                         |
+                {32{
+                    mem_alu_res[1:0] == 2'b01
+                }} & {ex_mem_rt_data[31:24], data_ram_rdata[31: 8]}  |
+                {32{
+                    mem_alu_res[1:0] == 2'b10
+                }} & {ex_mem_rt_data[31:16], data_ram_rdata[31:16]} |
+                {32{
+                    mem_alu_res[1:0] == 2'b11
+                }} & {ex_mem_rt_data[31: 8], data_ram_rdata[31:24]} ;
+        end
+        default: begin
+            mem_r_data  = 32'h0;
+        end
+        endcase
+    end
     
     assign mem_w_reg_ena    = ex_mem_w_reg_ena;
     assign mem_wb_reg_sel   = ex_mem_wb_reg_sel;
