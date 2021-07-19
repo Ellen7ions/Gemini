a7f4ac6909d088224d3dd04165f92dc9704524a1
Ellen7ions <lzz765177255@gmail.com>
2021-07-19 22:47:07 +0800

    Reset All From Birthday.

--- rtl/cache/LFSR.v
+++ rtl/cache/LFSR.v
@@ -11,7 +11,7 @@ module LFSR #(
     reg [11 :0] x;
     always @(posedge clk) begin
         if (rst) begin
-            x <= 12'b0101_0010_1000;
+            x <= {4'd5, 4'd2, 4'd8};
         end else begin
             x <= {x[0] ^ x[1], x[11 :1]};
         end
